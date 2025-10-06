c90ab5444462bf563d2e8ee271575eaef66dabc6
Sambit99 <sambitkumar75@gmail.com>
2025-10-06 22:06:49 +0530

    feat: Implement createPatient method in PatientService

--- patient-service/src/main/java/com/pms/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pms/patientservice/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.pms.patientservice.service;
 
+import com.pms.patientservice.dto.PatientRequestDTO;
 import com.pms.patientservice.dto.PatientResponseDTO;
 import com.pms.patientservice.mapper.PatientMapper;
 import com.pms.patientservice.model.Patient;
@@ -19,5 +20,12 @@ public class PatientService {
 
         return patients.stream()
                 .map(PatientMapper::toDTO).toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        Patient patient = patientRepository.save(
+                PatientMapper.toEntity(patientRequestDTO));
+
+        return PatientMapper.toDTO(patient);
     }
 }
