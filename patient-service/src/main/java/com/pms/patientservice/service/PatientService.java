f69c2e75796480f7b87b0ddd34a2d0e494044361
Sambit99 <sambitkumar75@gmail.com>
2025-10-08 22:22:46 +0530

    feat: Implement email existence check in createPatient method

--- patient-service/src/main/java/com/pms/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pms/patientservice/service/PatientService.java
@@ -2,11 +2,14 @@ package com.pms.patientservice.service;
 
 import com.pms.patientservice.dto.PatientRequestDTO;
 import com.pms.patientservice.dto.PatientResponseDTO;
+import com.pms.patientservice.exception.EmailAlreadyExistException;
 import com.pms.patientservice.mapper.PatientMapper;
 import com.pms.patientservice.model.Patient;
 import com.pms.patientservice.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -23,6 +26,12 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        boolean isEmailExist = patientRepository.existsByEmail(patientRequestDTO.getEmail());
+
+        if(isEmailExist){
+            throw new EmailAlreadyExistException("A patient with this e-mail already exists " + patientRequestDTO.getEmail());
+        }
+
         Patient patient = patientRepository.save(
                 PatientMapper.toEntity(patientRequestDTO));
 
