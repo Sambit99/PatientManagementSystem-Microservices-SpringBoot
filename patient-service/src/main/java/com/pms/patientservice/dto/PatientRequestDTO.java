ef5e2d6c9d43a12e4b50cdcd29723347d7af42e1
Sambit99 <sambitkumar75@gmail.com>
2025-10-09 20:34:50 +0530

    feat: Update PatientRequestDTO to enforce registered date validation group

--- patient-service/src/main/java/com/pms/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pms/patientservice/dto/PatientRequestDTO.java
@@ -1,5 +1,6 @@
 package com.pms.patientservice.dto;
 
+import com.pms.patientservice.dto.validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -21,6 +22,6 @@ public class PatientRequestDTO {
     @NotBlank(message = "Address is required")
     private String address;
 
-    @NotBlank(message = "Registered date is required")
+    @NotBlank(groups = CreatePatientValidationGroup.class, message = "Registered date is required")
     private String registeredDate;
 }
