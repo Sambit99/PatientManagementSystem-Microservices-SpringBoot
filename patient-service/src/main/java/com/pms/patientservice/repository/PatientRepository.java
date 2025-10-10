80c40c4fbb48886ab5f467cc54ad62f28383fb73
Sambit99 <sambitkumar75@gmail.com>
2025-10-10 20:38:43 +0530

    feat: Add existsByEmailAndIdNot method to PatientRepository for email uniqueness validation

--- patient-service/src/main/java/com/pms/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/pms/patientservice/repository/PatientRepository.java
@@ -4,9 +4,11 @@ import com.pms.patientservice.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
     boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, UUID id);
 }
