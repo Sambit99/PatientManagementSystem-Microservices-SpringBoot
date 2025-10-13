5df0395a4ff43e9a50b4c807f52114f12f71a31f
Sambit99 <sambitkumar75@gmail.com>
2025-10-13 20:46:54 +0530

    feat: Implement deletePatient method to remove a patient by ID

--- patient-service/src/main/java/com/pms/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pms/patientservice/service/PatientService.java
@@ -61,4 +61,11 @@ public class PatientService {
         Patient updatedPatient = patientRepository.save(patient);
         return PatientMapper.toDTO(updatedPatient);
     }
+
+    public void deletePatient(UUID id){
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new PatientNotFoundException("Patient not found with ID : "+ id));
+
+        patientRepository.delete(patient);
+    }
 }
