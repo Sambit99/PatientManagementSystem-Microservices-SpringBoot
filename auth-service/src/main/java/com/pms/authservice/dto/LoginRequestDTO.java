ce3dc10d3015b0e22c725157f8092b2cdfe01edf
Sambit99 <sambitkumar75@gmail.com>
2025-12-01 20:22:47 +0530

    feat: Create LoginRequestDTO with validation for email and password

---
+++ auth-service/src/main/java/com/pms/authservice/dto/LoginRequestDTO.java
@@ -0,0 +1,18 @@
+package com.pms.authservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Value;
+
+@Data
+public class LoginRequestDTO {
+    @NotBlank(message = "E-mail is required")
+    @Email(message = "E-mail should be a valid e-mail address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 15, message = "Password size should be between 8-15 characters long")
+    private String password;
+}
