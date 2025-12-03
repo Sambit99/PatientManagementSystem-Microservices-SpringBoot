d3e7fe1cb7632528f303c03775c8e2498e4f783e
Sambit99 <sambitkumar75@gmail.com>
2025-12-03 19:47:51 +0530

    feat: Add token validation method to AuthService

--- auth-service/src/main/java/com/pms/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pms/authservice/service/AuthService.java
@@ -2,6 +2,7 @@ package com.pms.authservice.service;
 
 import com.pms.authservice.dto.LoginRequestDTO;
 import com.pms.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,15 @@ public class AuthService {
                 .map(user -> jwtUtil.generateToken(user.getEmail(), user.getRole().toString()));
 
         return token;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        }catch (JwtException e){
+            return false;
+        }
     }
 
 }
