d301a15553a19777fe3e6c287071b3c961045b8c
Sambit99 <sambitkumar75@gmail.com>
2025-12-10 19:38:27 +0530

    feat: Add integration test for authentication endpoint

---
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,40 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+class AuthIntegrationTest {
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://localhost:4000";
+    }
+
+    @Test
+    void shouldReturnOkWithValidToken() {
+        String loginPayload = """
+                    {
+                        "email": "testuser@test.com",
+                        "password": "password123"
+                    }
+                """;
+
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .body("data", notNullValue())
+                .body("data.token",notNullValue())
+                .extract()
+                .response();
+
+        System.out.println("Response: " + response.getBody().asString());
+    }
+
+
+}
