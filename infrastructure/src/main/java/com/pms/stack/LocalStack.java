90049059040e90434519148b58fa97b53416158c
Sambit99 <sambitkumar75@gmail.com>
2026-01-07 20:25:54 +0530

    feat: Enhance LocalStack to create a VPC for infrastructure setup

--- infrastructure/src/main/java/com/pms/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pms/stack/LocalStack.java
@@ -1,10 +1,23 @@
 package com.pms.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
+    private final Vpc vpc;
+
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
+
+        this.vpc = createVpc();
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder
+                .create(this, "PatientManagementVpc")
+                .vpcName("PatientManagementVpc")
+                .maxAzs(2)
+                .build();
     }
 
     public static void main(String[] args) {
